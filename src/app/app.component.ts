e9a630331ee1348a271346347dadbdf53ebef551
catherinekimani <catherinekimani882@gmail.com>
2022-11-10 17:00:47 +0300

    Update app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'angular-observables';
+
+  myObservable = new Observable((observer) => {
+    console.log('observable starts')
+    setTimeout(() => { observer.next('1') }, 1000)
+    setTimeout(()=>{observer.next('2')},2000)
+    setTimeout(() => { observer.next('3') }, 3000)
+    // setTimeout(()=>{observer.error(new Error('something went wrong'))},3000)
+    setTimeout(() => { observer.next('4') }, 4000)
+    setTimeout(() => { observer.next('5') }, 5000)
+    setTimeout(()=>{observer.complete()},4000)
+    
+    
+    
+    // observer.next('1')
+    // observer.next('2')
+    // observer.next('3')
+    // observer.next('4')
+
+  });
+  ngOnInit() {
+    this.myObservable.subscribe((val) => {
+      console.log(val);
+    }, (error) => {
+      // alert(error.message)
+    }, () => {
+      // alert('observable has completed emitting all values')
+    })
+  }
 }
