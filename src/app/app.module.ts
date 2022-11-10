0cfc6229b77764caeb8bccb948e87619e0017b14
catherinekimani <catherinekimani882@gmail.com>
2022-11-10 16:57:51 +0300

    Import Forms module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,29 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { ContainerComponent } from './Container/container.component';
+import { NavComponent } from './nav/nav.component';
+import { HeaderComponent } from './header/header.component';
+import { NotificationComponent } from './notification/notification.component';
+import { SearchComponent } from './search/search.component';
+
+import { FormsModule } from '@angular/forms';
+import { ProductsComponent } from './products/products.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ContainerComponent,
+    NavComponent,
+    HeaderComponent,
+    NotificationComponent,
+    SearchComponent,
+    ProductsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
